479822c3d65d1857dacc34bd7bdc3a10cafc07a4
arpit-png <156171125+arpit-png@users.noreply.github.com>
2024-09-28 13:29:08 +0530

    Add files via upload

---
+++ DAY1-CWC23QUALIF.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int X;
+	cin>>X;
+	if(X>=12){
+	    cout<<"Yes";
+	}
+	else{
+	    cout<<"No";
+	}
+	
+	return 0;
+}
+
