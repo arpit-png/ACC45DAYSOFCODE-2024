c10840aab2ce65dd8a697e217964e000161ce61d
arpit-png <156171125+arpit-png@users.noreply.github.com>
2024-09-30 23:24:19 +0530

    Add files via upload

---
+++ DAY3-OFFICE.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T,X,Y;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X>>Y;
+	    cout<<4*X+Y<<endl;
+	}
+	return 0;
+}
