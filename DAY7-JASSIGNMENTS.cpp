e2d0089edf21a3465f080f770497e0b657bb7f25
arpit-png <156171125+arpit-png@users.noreply.github.com>
2024-10-04 21:57:49 +0530

    Add files via upload

---
+++ DAY7-JASSIGNMENTS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T,X;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X;
+	    if((X+3)<=10)
+	    cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
