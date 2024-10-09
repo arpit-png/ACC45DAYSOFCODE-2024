193f6932e0870699a25782b8a7e93b7b7e4c94b8
arpit-png <156171125+arpit-png@users.noreply.github.com>
2024-10-09 21:52:02 +0530

    Add files via upload

---
+++ DAY12-FLIPCARDS.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+ 	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    if(x==0 || n==x)
+	    cout<<0<<endl;
+	    else if(n-x>x)
+	    cout<<x<<endl;
+	    else
+	    cout<<n-x<<endl;
+	    
+	    
+	}
+	return 0;
+}
+
+
