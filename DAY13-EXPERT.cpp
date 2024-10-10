0f198257bf09b6ca9747f34aa04c09d281083ab1
arpit-png <156171125+arpit-png@users.noreply.github.com>
2024-10-10 21:09:46 +0530

    Add files via upload

---
+++ DAY13-EXPERT.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+	cin >> t;
+	while(t--)
+	{
+	    float x, y;
+	    cin >> x >> y;
+	    if(y >= (x/2.0))
+	    {
+	        cout << "YES\n";
+	    }
+	    else
+	    {
+	        cout << "NO\n";
+	    }
+	}
+	return 0;
+}
