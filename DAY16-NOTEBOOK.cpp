d5f1ecfdebe2d2a9a27dc85ef054e2d6a9d8f84b
ARPIT SHUKLA <156171125+arpit-png@users.noreply.github.com>
2024-10-13 15:58:17 +0530

    Add files via upload

---
+++ DAY16-NOTEBOOK.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+    cin >> t;
+    while(t--){
+
+        double pulp ; 
+        
+        cin >> pulp ;
+            cout << ceil((pulp * 1000) / 100)  << endl;
+            
+            // cout << ceil(pulp * 10) << endl;
+    }
+    return 0;
+}
