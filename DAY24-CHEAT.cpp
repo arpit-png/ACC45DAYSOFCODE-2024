b05e529df791451cc2917f370ee21a3097e09106
ARPIT SHUKLA <156171125+arpit-png@users.noreply.github.com>
2024-10-26 12:57:38 +0530

    Add files via upload

---
+++ DAY24-CHEAT.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int y;
+	cin>>y;
+	while(y--)
+	{
+	    int f;
+	    cin>>f;
+	    cout<<ceil(float(f-1)/float(7))<<endl;
+	}
+	return 0;
+}
